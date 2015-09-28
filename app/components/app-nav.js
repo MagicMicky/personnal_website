66473281fd09bf86f0da2c2af3be8d60367f5ee6
Mickael Goubin <micky0304@gmail.com>
2015-09-28 14:51:55 +0200

    Move app navigation js to right file

--- app/components/app-nav.js
+++ app/components/app-nav.js
@@ -2,5 +2,24 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 	tagName: 'div',
-	classNames: ['container']
+	classNames: ['container'],
+	_initializeOnePageNav: function() {
+		Ember.$('#mainNav').onePageNav({
+			currentClass: 'active',
+			changeHash: false,
+			scrollSpeed: 950,
+			scrollThreshold: 0.2,
+			filter: '',
+			easing: 'swing',
+			begin: function() {
+				//I get fired when the animation is starting
+			},
+			end: function() {
+				//I get fired when the animation is ending
+			},
+			scrollChange: function($currentListItem) {
+				//I get fired when you enter a section and I pass the list item of the section
+			}
+		});
+	}.on('didInsertElement')
 });
